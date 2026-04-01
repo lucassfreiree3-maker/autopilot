5170d42d203365702e20168344137992f39ac9fb
lucassfreiree <lucaslima4132@gmail.com>
2026-04-01 03:03:52 +0000

    backup: 20260401-024956 state/workspaces/ws-default/fetched-agent-src-routes-apis.ts [skip ci]

---
+++ snapshots/20260401-024956/state/workspaces/ws-default/fetched-agent-src-routes-apis.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import MetricsAPI from "./metrics.api";
+import RootAPI from "./root.api";
+import CriaPost from "./create-cronjob";
+import AgentRoutes from "./infoAgent";
+import ErrorAgentRoutes from "./errorAgent";
+import ExecuteRoutes from "./execute";
+import RegisterRoutes from "./register";
+
+// ADICIONADO: rota que recebe os logs do Agent e expõe consulta por execId
+import AgentExecuteLogsAPI from "./agent-execute-logs.api";
+
+// ADICIONADO: rota de callback para resultados de cronjob (#930217)
+import CronjobCallbackAPI from "./cronjob-callback";
+
+export class ApisRouter {
+  constructor(router: Router, ...args: unknown[]) {
+    new MetricsAPI(router, ...(args as []));
+    new RootAPI(router, ...(args as []));
+    new CriaPost(router, ...(args as []));
+    new AgentRoutes(router, ...(args as []));
+    new ErrorAgentRoutes(router, ...(args as []));
+    new ExecuteRoutes(router, ...(args as []));
+    new RegisterRoutes(router, ...(args as []));
+
+    // monta a nova rota SEM remover as existentes
+    new AgentExecuteLogsAPI(router, ...(args as []));
+
+    // monta rota de callback para resultados de cronjob
+    new CronjobCallbackAPI(router, ...(args as []));
+  }
+}
+
+export default ApisRouter;
